9c446f77b169732eaa28f9bf3de841e14ea9631d
Erick Luan <flinprice1308ft@gmail.com>
2021-09-07 10:43:18 -0300

    changing the logic

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 const ranges = document.querySelectorAll('.square');
 const square = document.querySelector('.result');
 
-ranges.forEach(range => {
-    range.addEventListener('change', event => {
-        if (range.name === "0") square.style.cssText += `border-top-left-radius: ${event.target.value}px;`
-        else if (range.name === "1") square.style.cssText += `border-top-right-radius: ${event.target.value}px;`
-        else if (range.name === "2") square.style.cssText += `border-bottom-right-radius: ${event.target.value}px;`
-        else if (range.name === "3") square.style.cssText += `border-bottom-left-radius: ${event.target.value}px;`
-    })
-})
+function rangeChange(event) {
+    if (event.target.name === "0") square.style.cssText += `border-top-left-radius: ${event.target.value}px;`
+    else if (event.target.name === "1") square.style.cssText += `border-top-right-radius: ${event.target.value}px;`
+    else if (event.target.name === "2") square.style.cssText += `border-bottom-right-radius: ${event.target.value}px;`
+    else if (event.target.name === "3") square.style.cssText += `border-bottom-left-radius: ${event.target.value}px;`
+}
